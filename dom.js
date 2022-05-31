ba12998400e01640a171c86988cd7bbf9e28e132
Abs-def <archisman64@gmail.com>
2022-05-31 23:50:34 +0530

    task_3_done

---
+++ dom.js
@@ -0,0 +1,20 @@
+//console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.forms);
+// console.log(document.links);
+let headerTitle = document.getElementById('header-title');
+//console.log(headerTitle.innerText);
+
+//headerTitle.textContent = 'archisman';
+// headerTitle.innerHTML = '<h3>archiesaha</h3>';
+headerTitle.style.borderBottom = 'solid 2px black';
+
+let title = document.getElementsByClassName('title');
+//console.log(title[0]);
+title[0].style.fontWeight = '900';
+title[0].style.color = 'green';
