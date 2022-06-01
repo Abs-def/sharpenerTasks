07cbda8f5f8a2f93147045d8546b4192abc7285d
Abs-def <archisman64@gmail.com>
2022-06-01 20:52:31 +0530

    task_4_done

--- dom.js
+++ dom.js
@@ -7,14 +7,20 @@
 // console.log(document.body);
 // console.log(document.forms);
 // console.log(document.links);
-let headerTitle = document.getElementById('header-title');
+//let headerTitle = document.getElementById('header-title');
 //console.log(headerTitle.innerText);
 
 //headerTitle.textContent = 'archisman';
 // headerTitle.innerHTML = '<h3>archiesaha</h3>';
-headerTitle.style.borderBottom = 'solid 2px black';
+//headerTitle.style.borderBottom = 'solid 2px black';
 
-let title = document.getElementsByClassName('title');
-//console.log(title[0]);
-title[0].style.fontWeight = '900';
-title[0].style.color = 'green';
+// let title = document.getElementsByClassName('title');
+// console.log(title[0]);
+// title[0].style.fontWeight = '900';
+// title[0].style.color = 'green';
+let items = document.getElementsByClassName('list-group-item');
+console.log(items);
+items[2].style.backgroundColor = 'green';
+for(let i=0; i<items.length; i++){
+    items[i].style.fontWeight = 'bold';
+}
