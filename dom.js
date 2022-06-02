9152bb129f57544f7c9f21281e484a9cebf7c2ba
Abs-def <archisman64@gmail.com>
2022-06-02 11:21:25 +0530

    task_6_querySelector

--- dom.js
+++ dom.js
@@ -18,13 +18,18 @@
 // console.log(title[0]);
 // title[0].style.fontWeight = '900';
 // title[0].style.color = 'green';
-let itemsByClass = document.getElementsByClassName('list-group-item');
+// let itemsByClass = document.getElementsByClassName('list-group-item');
 
-for(let i=0; i<itemsByClass.length; i++){
-    itemsByClass[i].style.fontWeight = 'bold';  //does not apply to the li element without class.
-}
-let itemsByTag = document.getElementsByTagName('li');
+// for(let i=0; i<itemsByClass.length; i++){
+//     itemsByClass[i].style.fontWeight = 'bold';  //does not apply to the li element without class.
+// }
+// let itemsByTag = document.getElementsByTagName('li');
 
-for(let i=0; i<itemsByTag.length; i++){
-    itemsByTag[i].style.color = 'red'; //applies to all elements with the tag 'li'.
-}
+// for(let i=0; i<itemsByTag.length; i++){
+//     itemsByTag[i].style.color = 'red'; //applies to all elements with the tag 'li'.
+// }
+//QUERY SELECTOR//
+let item2 = document.querySelector('.list-group-item:nth-child(2)');
+item2.style.background = 'green';
+let item3 = document.querySelector('.list-group-item:nth-child(3)');
+item3.style.visibility = 'hidden';
