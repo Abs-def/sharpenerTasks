80d64b1e837a529df05ccdd8ceb9bfae1f4cef98
Abs-def <archisman64@gmail.com>
2022-06-06 16:22:35 +0530

    task_8

--- dom.js
+++ dom.js
@@ -1,86 +1,47 @@
-//console.dir(document);
-// console.log(document.domain);
-// console.log(document.URL);
-// console.log(document.title);
-// console.log(document.doctype);
-// console.log(document.head);
-// console.log(document.body);
-// console.log(document.forms);
-// console.log(document.links);
-//let headerTitle = document.getElementById('header-title');
-//console.log(headerTitle.innerText);
+let form = document.getElementById('addForm');
+let itemList = document.getElementById('items');
 
-//headerTitle.textContent = 'archisman';
-// headerTitle.innerHTML = '<h3>archiesaha</h3>';
-//headerTitle.style.borderBottom = 'solid 2px black';
+// Form submit event
+form.addEventListener('submit', addItem);
+//delete event;
+itemList.addEventListener('click', removeItem);
 
-// let title = document.getElementsByClassName('title');
-// console.log(title[0]);
-// title[0].style.fontWeight = '900';
-// title[0].style.color = 'green';
-// let itemsByClass = document.getElementsByClassName('list-group-item');
+function addItem(e){
+    e.preventDefault();
 
-// for(let i=0; i<itemsByClass.length; i++){
-//     itemsByClass[i].style.fontWeight = 'bold';  //does not apply to the li element without class.
-// }
-// let itemsByTag = document.getElementsByTagName('li');
+    // get input value;
+    let newItem = document.getElementById('item').value;
 
-// for(let i=0; i<itemsByTag.length; i++){
-//     itemsByTag[i].style.color = 'red'; //applies to all elements with the tag 'li'.
-// }
-//QUERY SELECTOR//
-// let item2 = document.querySelector('.list-group-item:nth-child(2)');
-// item2.style.background = 'green';
-// let item3 = document.querySelector('.list-group-item:nth-child(3)');
-// item3.style.visibility = 'hidden';
-// QUERYSELECTORALL //
-//let items = document.querySelectorAll('.list-group-item');
-//console.log(items);
-// items[1].style.color = 'green';
-// let oddItems = document.querySelectorAll('li:nth-child(odd)');
-// for(let i=0; i<oddItems.length; i++){
-//     oddItems[i].style.background = 'green';
-// }
-// let items = document.querySelector('#items');
-//console.log(items.parentNode);
-//items.parentNode.style.background = 'grey';
-// console.log(items.parentElement.parentElement);
-// items.parentElement.parentElement.parentElement.style.background = 'yellow';
-// console.log(items.childNodes);
-// console.log(items.children);
-// console.log(items.children[2]);
-// items.children[2].textContent = 'archie';
-// console.log(items.firstElementChild);
-//items.firstElementChild.textContent = 'archie';
-// console.log(items.previousElementSibling);
-// items.previousElementSibling.style.color = 'blue';
+    // create new li element;
+    let li = document.createElement('li');
+    li.className = 'list-group-item';
+    // add text node with input value;
+    li.appendChild(document.createTextNode(newItem));
 
-//  CREATING NEW ELEMENT    //
-let newDiv = document.createElement('div');
+    // create delete button;
+    let deleteBtn = document.createElement('button');
+    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+    //append text node to delete button;
+    deleteBtn.appendChild(document.createTextNode('delete'));
 
-newDiv.setAttribute('class', 'newHello');
-newDiv.setAttribute('id', 'hello1');
-newDiv.title = 'hello div';
-//  CREATE AND INSERT TEXT NODE //
-let newText = document.createTextNode('hello world');
-newDiv.appendChild(newText);
-console.log(newDiv);
-// adding new div before item lister //
-let container = document.querySelector('header .container');
-let h1 = document.querySelector('header h1');
+    //create edit button
+    let editBtn = document.createElement('button');
+    editBtn.className = 'btn btn-sm float-right';
+    editBtn.appendChild(document.createTextNode('edit'));
 
-container.insertBefore(newDiv, h1);
+    //append buttons to li
+    li.appendChild(deleteBtn);
+    li.appendChild(editBtn);
 
+    //append li to list
+    itemList.appendChild(li);
+}
 
-//  adding new div before item 1    //
-let newDiv1 = document.createElement('div');
-newDiv1.setAttribute('class', 'newHello1');
-newDiv1.setAttribute('id', 'newhello1');
-newDiv1.title = 'hello div1';
-let newText1 = document.createTextNode('hello world');
-newDiv1.appendChild(newText1);
-
-let container1 = document.querySelector('div .list-group');
-let h2 = document.querySelector('div li');
-
-container1.insertBefore(newDiv1, h2);
+function removeItem(e){
+    if(e.target.classList.contains('delete')){
+        if(confirm('Are you sure?')){
+            let li = e.target.parentElement;
+            itemList.removeChild(li);
+        }
+    }
+}
